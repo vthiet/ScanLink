390e31baa0ab9c2a68bf19aa584c510cead40657
Pluf_nluer <23130240@st.hcmuaf.edu.vn>
2026-06-21 15:05:39 +0700

    build: fix Firebase auth dependency conflict and clean up MainActivity

--- app/src/main/java/com/example/scanlink/MainActivity.kt
+++ app/src/main/java/com/example/scanlink/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.scanlink
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.scanlink.core.ui.theme.ScanLinkTheme
-import com.example.scanlink.features.authentication.domain.repositories.IAuthRepository
 import com.example.scanlink.features.authentication.presentation.register.RegisterViewModel
 import com.example.scanlink.navigation.AppNavigation
-import com.example.scanlink.navigation.MainGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,18 +22,7 @@ class MainActivity : ComponentActivity() {
                     registerViewModel = viewModel
                 )
             }
-//            ScanLinkTheme {
-//                MainGraph(,Modifier)
-//            }
         }
 
     }
 }
-
-//@Preview(showBackground = true, showSystemUi = true)
-//@Composable
-//fun GreetingPreview() {
-//    ScanLinkTheme {
-//        MainScreen()
-//    }
-//}
