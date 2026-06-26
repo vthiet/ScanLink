f431ede7c3272a404f5b03b559e46b59ebb45318
Pluf_nluer <23130240@st.hcmuaf.edu.vn>
2026-06-26 15:46:40 +0700

    fix: chinh sua flow tu preview -> ocr

--- app/src/main/java/com/example/scanlink/MainActivity.kt
+++ app/src/main/java/com/example/scanlink/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.scanlink
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.scanlink.core.ui.theme.ScanLinkTheme
 import com.example.scanlink.navigation.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
+import org.opencv.android.OpenCVLoader
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val isOpenCvLoaded = OpenCVLoader.initDebug()
+
+        if (isOpenCvLoaded) {
+            Log.d("OpenCV", "OpenCV loaded successfully")
+        } else {
+            Log.e("OpenCV", "OpenCV load failed")
+        }
 
         setContent {
             ScanLinkTheme {
